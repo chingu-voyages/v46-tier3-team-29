ebdc6cd0f79df341b22cfd2d56c97a0cda5d712d
frugalcodes <samueligwe63@gmail.com>
2023-10-17 16:13:33 +0100

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,19 @@
-const About = () => <h1>About</h1>
+import React from 'react'
+
+const About = () => {
+  return (
+    
+    <div className="bg-white p-6 rounded-lg container mx-auto p-3">
+    <h1 className="text-3xl font-semibold mb-4 text-center">About Running Club</h1>
+    <p className="mb-4">Welcome to Running Club, a community of passionate runners. Our mission is to promote an active and healthy lifestyle through running. Whether you are a seasoned marathoner or just starting your running journey, we are here to support you every step of the way.</p>
+    <p className="mb-4">At Running Club, we organize various events and training programs to help you achieve your running goals. We believe that running is not just a sport but a way of life. It is an opportunity to challenge yourself, connect with like-minded individuals, and enjoy the outdoors.</p>
+    <p className="mb-4">Our website features a home page, events page, and a contact page, all designed to provide you with essential information and resources. It is also responsive and mobile-friendly, ensuring that you can access our content from any device.</p>
+    <p className="mb-4">Join us on this incredible journey of self-discovery, fitness, and camaraderie. We look forward to running with you!</p>
+    
+    <button className="bg-primary text-white font-semibold py-3 px-12 rounded-full block mx-auto">Join</button>
+</div>
+
+  )
+}
 
 export default About
